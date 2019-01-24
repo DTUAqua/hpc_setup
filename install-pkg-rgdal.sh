aa78d93c6303ee242b2292b2303111c72a5719b5
kaskr <kaskr@dtu.dk>
2019-01-24 11:06:58 +0100

    rgdal install not working

--- install-pkg-rgdal.sh
+++ install-pkg-rgdal.sh
@@ -2,8 +2,10 @@
 
 REPOS="https://cloud.r-project.org"
 
-export LD_LIBRARY_PATH=/appl/proj/4.9.3/lib:$LD_LIBRARY_PATH
-export PKG_CPPFLAGS="-I/appl/proj/4.9.3/include $CPPFLAGS"
-export PKG_LIBS="-Wl,-rpath=/appl/proj/4.9.3/lib -L/appl/proj/4.9.3/lib -lproj $LDFLAGS"
+module load gdal/2.1.3
+
+export LD_LIBRARY_PATH=/appl/proj/5.1.0/lib:$LD_LIBRARY_PATH
+export PKG_CPPFLAGS="-I/appl/proj/5.1.0/include $CPPFLAGS"
+export PKG_LIBS="-Wl,-rpath=/appl/proj/5.1.0/lib -L/appl/proj/5.1.0/lib -lproj $LDFLAGS"
 
 echo "install.packages('rgdal',repos='$REPOS')" | R --vanilla
