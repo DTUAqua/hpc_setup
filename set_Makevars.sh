51cd85c5fb6dafea528722004449f959eb000a2f
kaskr <kaskr@dtu.dk>
2019-01-24 10:32:52 +0100

    Silent RcppEigen warnings

--- set_Makevars.sh
+++ set_Makevars.sh
@@ -3,5 +3,5 @@
 mkdir -p ${HOME}/.R
 rm -f ${HOME}/.R/Makevars
 
-echo "CPPFLAGS=-DEIGEN_USE_MKL_ALL -DBOOST_PHOENIX_NO_VARIADIC_EXPRESSION" `R CMD config CPPFLAGS` > ${HOME}/.R/Makevars
+echo "CPPFLAGS=-DEIGEN_USE_MKL_ALL -DBOOST_PHOENIX_NO_VARIADIC_EXPRESSION -Wno-ignored-attributes" `R CMD config CPPFLAGS` > ${HOME}/.R/Makevars
 ## echo "CXXFLAGS=-Ofast" >> ${HOME}/.R/Makevars
